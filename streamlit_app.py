aab147b184544545a6f30ea471e10e75a3b32b07
Vishnuram <117495092+thisisvishnu567@users.noreply.github.com>
2025-12-24 06:31:09 +0530

    Simplify GDP data description in streamlit_app.py
    Removed unnecessary information about data limitations.

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,9 +66,7 @@ gdp_df = get_gdp_data()
 '''
 # :earth_americas: GDP dashboard
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website.
 '''
 
 # Add some spacing
